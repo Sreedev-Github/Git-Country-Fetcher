9746b68a72e1284c036214f65cb427d278c4e9b6
Sreedev-Github <sreedev0612@gmail.com>
2024-03-23 09:39:31 +0530

    Some code refactoring in script file

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ const cardContainer = document.querySelector("#cards");
 const selectText = document.querySelector('.optionText')
 
 dropdownBtn.addEventListener("click", (e) => {
-  if (countries.style.display === "block") {
-    countries.style.display = "none";
-  } else {
-    countries.style.display = "block";
-  }
+  // Used ternary operator for smaller code
+countries.style.display = countries.style.display === 'block' ? 'none' : 'block';
 });
 
 countries.addEventListener("click", (e) => {
-  dropdown.setAttribute("value", e.target.innerHTML);
-  countries.style.display = "none";
-  fetchData(e.target.innerHTML);
-  selectText.style.display = 'none'
+    if (e.target.tagName === 'A') { // Ensure user is clicking on a link
+        dropdownBtn.setAttribute("value", e.target.innerHTML);
+        countries.style.display = "none";
+        fetchData(e.target.innerHTML);
+        selectText.style.display = 'none';
+      }
 });
 
 fetch("https://restcountries.com/v3.1/all")
@@ -40,29 +39,23 @@ fetch("https://restcountries.com/v3.1/all")
     alert("Sorry there seems to be some issue with the servers");
   });
 
-async function fetchData(location, pageNo) {
-  fetch(`https://api.github.com/search/users?q=location:${location}&page=1&per_page=18`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-        if(cardContainer.childElementCount != 0){
-            for (let i = 0; i < cardContainer.childElementCount; i++) {
-                cardContainer.removeChild(cardContainer.firstChild)
-            }
-      }
-      const users = data.items; // Returns Array with objects      
-      users.forEach((user) => {
-        const newCard = document.createElement("div");
-        newCard.className = "card";
-        newCard.innerHTML = `
-                <img src="${user.avatar_url}" class="card-img-top" alt="..." />
-                <div class="card-body">
-                <h5 class="card-title">${user.login}</h5>
-                <a href="https://github.com/${user.login}" class="btn btn-primary">Visit Profile</a>
-                `;
-        cardContainer.appendChild(newCard);
-      });
-    })
-    .catch((err) => console.log(err));
+async function fetchData(location) {
+  try {
+    const response = await fetch(`https://api.github.com/search/users?q=location:${location}&page=1&per_page=18`);
+    const data = await response.json();
+    cardContainer.innerHTML = ''; // Clear existing cards
+    data.items.forEach(user => {
+      const newCard = document.createElement("div");
+      newCard.className = "card";
+      newCard.innerHTML = `
+        <img src="${user.avatar_url}" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">${user.login}</h5>
+          <a href="https://github.com/${user.login}" class="btn btn-primary">Visit Profile</a>
+        `;
+      cardContainer.appendChild(newCard);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
